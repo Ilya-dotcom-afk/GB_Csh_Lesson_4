27c5a243fea3edf4159d80f514e6f7bb79718945
Ilya <ilya_rogachev_89@mail.ru>
2022-03-26 12:34:23 +0300

    Task_2 ready

---
+++ 2/Program.cs
@@ -0,0 +1,21 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+int[] array = new int[123];
+int number = 0;
+Random rand = new Random();
+void FillArray (int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(1, 1000);
+    }
+}
+FillArray(array);
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i] >= 10 && array[i] <= 99)
+    {
+        number++;
+    }
+}
+Console.WriteLine("количество элементов из отрезка [10,99] равно " + number);
+
